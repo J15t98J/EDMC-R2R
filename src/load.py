e8589535b9b55e8956cead45ea4d68864603ad8b
Joshua Tyler-Jones <joshua@tylerjon.es>
2020-12-13 22:14:43 +0000

    ttk -> tk to fix theme

--- src/load.py
+++ src/load.py
@@ -1,10 +1,10 @@
 import logging
 import os
-import tkinter as tk
-import tkinter.filedialog
+
 from csv import DictReader
 from logging import Logger
-from tkinter import ttk
+from tkinter import *
+from tkinter import filedialog
 from typing import Optional, Dict, Any
 
 import myNotebook as nb
@@ -35,30 +35,30 @@ plugin_name: str = os.path.basename(os.path.dirname(__file__))
 log = setup_logging()
 current_r2r: list = []
 index: int = 0
-root: tk.Frame
-current_window: tk.Frame = None
+root: Frame
+current_window: Frame = None
 checkboxes = {}
 
 
-def goto_screen(screen: tk.Frame):
+def goto_screen(screen: Frame):
     global current_window
     if current_window is not None:
         current_window.destroy()
-    screen.grid(sticky=tk.E+tk.W)
+    screen.grid(sticky=E+W)
     theme.update(screen)
     current_window = screen
 
 
-def screen_init() -> tk.Frame:
-    init = ttk.Frame(root)
+def screen_init() -> Frame:
+    init = Frame(root)
     init.grid_columnconfigure(0, weight=1)
-    ttk.Button(init, text="Load R2R file", command=load_csv).grid(sticky=tk.E+tk.W)
+    Button(init, text="Load R2R file", command=load_csv).grid(sticky=E+W)
     return init
 
 
-def screen_progress() -> tk.Frame:
+def screen_progress() -> Frame:
     global index, checkboxes
-    progress = ttk.Frame(root)
+    progress = Frame(root)
     progress.grid_columnconfigure(1, weight=1)
     checkboxes = {}
 
@@ -94,27 +94,27 @@ def screen_progress() -> tk.Frame:
                 body_types[edsm_body["name"]] = edsm_body["subType"].lower()
     
         # system page header ([<] curr/total [>] \nSystem Name)
-        b_prev = ttk.Button(progress, text="<", command=prog_prev)
+        b_prev = Button(progress, text="<", width=8, command=prog_prev)
         b_prev.grid(row=0, column=0)
-        b_next = ttk.Button(progress, text=">", command=prog_next)
+        b_next = Button(progress, text=">", width=8, command=prog_next)
         b_next.grid(row=0, column=2)
-        ttk.Label(progress, text=f"{index+1}/{len(current_r2r)}").grid(row=0, column=1)
-        ttk.Label(progress, text=system["name"]).grid(row=1, columnspan=3)
+        Label(progress, text=f"{index+1}/{len(current_r2r)}").grid(row=0, column=1)
+        Label(progress, text=system["name"]).grid(row=1, columnspan=3)
     
         # prevent cycling round off the end of the list
         if index == 0:
-            b_prev.state(['disabled'])
+            b_prev['state'] = DISABLED
         if index == len(current_r2r)-1:
-            b_next.state(['disabled'])
+            b_next['state'] = DISABLED
     
         # a checkbox and label for each system body
-        bodies_container = ttk.Frame(progress)
+        bodies_container = Frame(progress)
         bodies_container.grid(row=2, columnspan=3)
         current_row = 0
         for body in system["bodies"]:
             fullname = f"{system['name']} {body}"
             
-            body_frame = ttk.Frame(bodies_container)
+            body_frame = Frame(bodies_container)
             body_frame.grid(row=current_row, sticky="W")
 
             if fullname in body_types:
@@ -122,20 +122,21 @@ def screen_progress() -> tk.Frame:
             else:
                 labeltext = body
 
-            state = tk.BooleanVar()
+            state = BooleanVar()
             state.trace_add("write", lambda *unused, x=body: mark_complete(x))
-            ttk.Checkbutton(body_frame, variable=state, command=lambda x=body: mark_complete(x)).pack(side="left")
-            label = ttk.Label(body_frame, text=labeltext)
+            Checkbutton(body_frame, variable=state, command=lambda x=body: mark_complete(x)).pack(side="left")
+            label = Label(body_frame, text=labeltext)
             label.pack(side="left")
 
             checkboxes[body] = {'state': state, 'label': label}
             current_row = current_row + 1
+            theme.update(body_frame)
 
     return progress
 
 
-def screen_complete() -> tk.Frame:
-    complete = ttk.Frame(root)
+def screen_complete() -> Frame:
+    complete = Frame(root)
     # TODO: put something on this page
     return complete
 
@@ -146,7 +147,7 @@ def load_csv():
     
     log.debug("Loading new CSV...")
     
-    with tk.filedialog.askopenfile(filetypes=[('CSV files', '*.csv')]) as csvfile:
+    with filedialog.askopenfile(filetypes=[('CSV files', '*.csv')]) as csvfile:
         # convert (system -> body) into (UNIQ system -> [body])
         for row in DictReader(csvfile):
             system = row['System Name']
@@ -170,7 +171,7 @@ def plugin_start3(plugin_dir: str) -> str:
     return "EDMC-R2R"
 
 
-def plugin_prefs(parent: nb.Notebook, cmdr: str, is_beta: bool) -> Optional[tk.Frame]:
+def plugin_prefs(parent: nb.Notebook, cmdr: str, is_beta: bool) -> Optional[Frame]:
     # TODO: plugin preferencees tab in the settings window
     pass
 
@@ -180,12 +181,12 @@ def prefs_changed(cmdr: str, is_beta: bool) -> None:
     pass
 
 
-def plugin_app(parent: tk.Frame) -> tk.Frame:
+def plugin_app(parent: Frame) -> Frame:
     global root
-    root = ttk.Frame(parent)
+    root = Frame(parent)
     root.grid_columnconfigure(0, weight=1)
 
-    ttk.Label(root, text="Road 2 Riches", anchor=tk.CENTER,).grid(sticky=tk.E+tk.W)
+    Label(root, text="Road 2 Riches", anchor=CENTER).grid(sticky=E+W)
     goto_screen(screen_init())
     return root
 
